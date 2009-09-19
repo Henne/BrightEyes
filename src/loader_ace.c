61cdc6375b283af3db54656b12a92e30bc07cc62
Henrik Kretzschmar <henne@nachtwindheim.de>
2009-09-19 22:30:51 +0200

    let the ACE loader do dumps

--- src/loader_ace.c
+++ src/loader_ace.c
@@ -21,6 +21,96 @@
 #include <dump.h>
 #include <format.h>
 
+static void do_ass(struct ace_header *ace, const char *buf, size_t len) {
+	struct ass_header ass;
+	unsigned long i, datalen;
+	char *pal;
+
+	if (len < sizeof(ass)) {
+		fprintf(stderr, "Buffer to small for ASS Header\n");
+		return;
+	}
+	ass.celwidth=get_sshort(buf);
+	ass.celheight=get_sshort(buf+2);
+	ass.amount=buf[4];
+	ass.playmode=buf[5];
+
+	printf("\t%03dx%03d\tAmount: %03u\tMode: %03u\n",
+			ass.celwidth,
+			ass.celheight,
+			ass.amount,
+			ass.playmode);
+
+
+	datalen=sizeof(ass);
+	for (i=0; i < ass.amount; i++) {
+		struct cel_header cel;
+
+		cel.size=get_sint(buf+datalen);
+		cel.xoffset=get_sshort(buf+datalen+4);
+		cel.yoffset=get_sshort(buf+datalen+6);
+		cel.width=get_sshort(buf+datalen+8);
+		cel.height=get_sshort(buf+datalen+10);
+		cel.compression=buf[datalen+12];
+		cel.action=buf[datalen+13];
+
+		printf("\t\tSize: %08x\tCompression: %x\n",
+				cel.size, cel.compression);
+
+		datalen+=sizeof(cel)+cel.size;
+	}
+
+	if (datalen + 2 > len) {
+		fprintf(stderr, "ASS File is to small for a palette\n");
+		return;
+	}
+
+
+	if ( len != datalen+256*3) {
+		fprintf(stderr, "ASS File has not the expected size\n");
+		return;
+	}
+	pal=(char*)buf+datalen;
+
+	datalen=sizeof(ass);
+	for (i=0; i < ass.amount; i++) {
+		struct cel_header cel;
+		char fname[100];
+		char *data;
+
+		cel.size=get_sint(buf+datalen);
+		cel.xoffset=get_sshort(buf+datalen+4);
+		cel.yoffset=get_sshort(buf+datalen+6);
+		cel.width=get_sshort(buf+datalen+8);
+		cel.height=get_sshort(buf+datalen+10);
+		cel.compression=buf[datalen+12];
+		cel.action=buf[datalen+13];
+
+		sprintf(fname, "CEL%03lu.TGA", i);
+		data=malloc(cel.width*cel.height);
+		switch (cel.compression) {
+			case 0x32: /* PP20 */
+				ppdepack(buf+datalen+sizeof(cel), data, cel.size, cel.width*cel.height);
+				dump_tga(fname, cel.width, cel.height, data, 256, pal);
+				break;
+			case 0x1: /* RLE */
+				un_rle(buf+datalen+sizeof(cel), data, cel.size);
+				dump_tga(fname, cel.width, cel.height, data, 256, pal);
+				break;
+			default:
+				fprintf(stdout, "Unknown ACE Compression %x\n",
+						cel.compression);
+		}
+		free(data);
+
+		datalen+=sizeof(cel)+cel.size;
+	}
+
+}
+
+static void do_seq(struct ace_header *ace, const char *buf, size_t len) {
+}
+
 void process_ace(const char *buf, size_t len) {
 	struct ace_header ace;
 
@@ -35,7 +125,7 @@ void process_ace(const char *buf, size_t len) {
 	}
 
 	strncpy(ace.label, buf, 4);
-	ace.version=get_ushort(buf+4);
+	ace.version=get_sshort(buf+4);
 	ace.sequences=buf[6];
 	ace.speed=buf[7];
 
@@ -61,5 +151,11 @@ void process_ace(const char *buf, size_t len) {
 	}
 
 	printf("Sequences: %03u\tSpeed: %02u\n", ace.sequences, ace.speed);
+
+	if (ace.sequences == 1)
+		do_ass(&ace, buf+sizeof(ace), len-sizeof(ace));
+	else
+		do_seq(&ace, buf+sizeof(ace), len-sizeof(ace));
+
 	return;
 }
