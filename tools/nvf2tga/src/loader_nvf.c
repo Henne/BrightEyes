46eee5de00c07669560f5bbfba9c8687bf804e61
Henrik Kretzschmar <henne@nachtwindheim.de>
2011-09-15 09:21:55 +0200

    nvf2tga: add detection of special NVF-Files

--- tools/nvf2tga/src/loader_nvf.c
+++ tools/nvf2tga/src/loader_nvf.c
@@ -21,6 +21,98 @@
 #include <dump.h>
 #include <format.h>
 
+struct struct_color {
+	signed char r, g, b;
+};
+
+static const struct struct_color pal_ggsts[32] = {
+	{0x00, 0x00, 0x00},
+	{0x38, 0x30, 0x28},
+	{0x30, 0x24, 0x1c},
+	{0x24, 0x18, 0x10},
+	{0x20, 0x14, 0x0c},
+	{0x18, 0x0c, 0x08},
+	{0x14, 0x08, 0x04},
+	{0x0c, 0x04, 0x00},
+	{0x3c, 0x38, 0x00},
+	{0x38, 0x28, 0x00},
+	{0x30, 0x1c, 0x00},
+	{0x2c, 0x14, 0x00},
+	{0x3c, 0x24, 0x00},
+	{0x3c, 0x0c, 0x00},
+	{0x3c, 0x00, 0x08},
+	{0x3c, 0x00, 0x20},
+	{0x00, 0x10, 0x00},
+	{0x00, 0x14, 0x00},
+	{0x00, 0x1c, 0x00},
+	{0x04, 0x20, 0x00},
+	{0x04, 0x28, 0x00},
+	{0x1c, 0x38, 0x3c},
+	{0x14, 0x2c, 0x34},
+	{0x0c, 0x1c, 0x30},
+	{0x04, 0x0c, 0x28},
+	{0x00, 0x00, 0x24},
+	{0x10, 0x10, 0x10},
+	{0x14, 0x14, 0x14},
+	{0x1c, 0x1c, 0x1c},
+	{0x24, 0x24, 0x24},
+	{0x30, 0x30, 0x30},
+	{0x3c, 0x3c, 0x3c},
+};
+
+/* This palette from V3.02de differs a bit from all the others */
+static const struct struct_color pal_ggsts_alt[32] = {
+	{0x30, 0x00, 0x00},
+	{0x30, 0x30, 0x00},
+	{0x00, 0x00, 0x30},
+	{0x00, 0x00, 0x00},
+	{0x38, 0x30, 0x28},
+	{0x30, 0x24, 0x1c},
+	{0x24, 0x18, 0x10},
+	{0x20, 0x14, 0x0c},
+	{0x18, 0x0c, 0x08},
+	{0x14, 0x08, 0x04},
+	{0x0c, 0x04, 0x00},
+	{0x3c, 0x38, 0x00},
+	{0x38, 0x28, 0x00},
+	{0x30, 0x1c, 0x00},
+	{0x2c, 0x14, 0x00},
+	{0x3c, 0x24, 0x00},
+	{0x3c, 0x0c, 0x00},
+	{0x3c, 0x00, 0x08},
+	{0x3c, 0x00, 0x20},
+	{0x00, 0x10, 0x00},
+	{0x00, 0x14, 0x00},
+	{0x00, 0x1c, 0x00},
+	{0x04, 0x20, 0x00},
+	{0x04, 0x28, 0x00},
+	{0x1c, 0x38, 0x3c},
+	{0x14, 0x2c, 0x34},
+	{0x0c, 0x1c, 0x30},
+	{0x04, 0x0c, 0x28},
+	{0x00, 0x00, 0x24},
+	{0x10, 0x10, 0x10},
+	{0x14, 0x14, 0x14},
+	{0x1c, 0x1c, 0x1c},
+};
+
+struct struct_special_nvf {
+	const char name[16];
+	const unsigned short length;
+	const unsigned short mode;
+	const unsigned short blocks;
+
+	const struct struct_color *pal;
+	const unsigned short colors;
+	const unsigned short first_color;
+};
+
+static const struct struct_special_nvf special_nvf[] = {
+	{ "GGSTS.NVF", 16771, 0x02, 0xa5, pal_ggsts, 0x20, 0x40 },
+	/* Terminator */
+	{ "", 0, 0, 0, NULL, 0, 0}
+};
+
 static void do_mode_0(unsigned short blocks, const char *buf, size_t len)
 {
 	unsigned long i;
@@ -144,27 +236,51 @@ static void do_mode_same(unsigned short blocks, const char *buf, size_t len,
 		printf("The buffer is to small to hold valid values\n");
 		return;
 	}
-	if (len < data_sum + 2) {
-		printf("The buffer is to small to hold a palette\n");
-		return;
+
+	/* This is the case in DSA1/ROA1 */
+	if (len == data_sum) {
+		for (i = 0; special_nvf[i].name[0] != '\0'; i++) {
+
+			if (special_nvf[i].length != len + 3)
+				continue;
+			if (special_nvf[i].mode != mode)
+				continue;
+			if (special_nvf[i].blocks != blocks)
+				continue;
+
+			printf("Seems to be %s. Using hardcoded palette\n",
+				special_nvf[i].name);
+
+			first_color = special_nvf[i].first_color;
+			colors = special_nvf[i].colors + first_color;
+			pal = (char*)special_nvf[i].pal;
+			printf("%d %d %p %p\n", colors, first_color, pal, &pal_ggsts);
+		}
+
+		if (pal == NULL) {
+			printf("Unknown NVF-file without palette\n");
+			return;
+		}
+	} else {
+
+		colors = get_ushort(buf + data_sum);
+		calc_len = data_sum + 2 + 3 * colors;
+
+		if (len != calc_len) {
+			printf("The data %lu has not the expected size %lu\n",
+			       len, calc_len);
+			return;
+		}
+
+		pal = (char *)(buf + data_sum + 2);
 	}
 
-	colors = get_ushort(buf + data_sum);
-	calc_len = data_sum + 2 + 3 * colors;
-
-	if (len != calc_len) {
-		printf("The data %lu has not the expected size %lu\n",
-		       len, calc_len);
-		return;
-	}
+	pdata = (char *)(buf + 4 + 4 * blocks);
 
 	if (mode == 2)
 		printf("NVF-Mode 2 (same size/PP20): %03d Pics\n", blocks);
 	else
 		printf("NVF-Mode 4 (same size/RLE): %03d Pics\n", blocks);
-
-	pal = (char *)(buf + data_sum + 2);
-	pdata = (char *)(buf + 4 + 4 * blocks);
 
 	data = malloc(x * y);
 	if (!data) {
