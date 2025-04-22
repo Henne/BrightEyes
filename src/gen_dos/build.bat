a9ffb45fafacdb68c58c909ffcaf148b0afbd30d
Henne <119480+Henne@users.noreply.github.com>
2025-04-22 21:20:46 +0200

    GEN_DOS: restructure build.bat

--- src/gen_dos/build.bat
+++ src/gen_dos/build.bat
@@ -3,15 +3,18 @@
 @REM They shall not change, so the are compared with every commit.
 @REM
 @REM -- here start the complete
+@REM BUILD THE LIBS
 BCC.EXE -mlarge -O  -c -2 -Z CDA_CODE.C
-BCC.EXE -mlarge -O  -c -2 -Z -IAIL GEN105DE.C
 BCC.EXE -mlarge -O  -c -2 -Z RANDOM.C
 TASM.EXE /os /z POWERP20.ASM POWERP20.OBJ
 TASM.EXE /os /z VGALIB.ASM VGALIB.OBJ
 TASM.EXE /m /w+ /ml /iAIL AIL\AIL.ASM AIL.OBJ
 TASM.EXE /os /z CDA_DATA.ASM CDA_DATA.OBJ
+
+@REM BUILD AND LINK THE BINARIES
+BCC.EXE -mlarge -O  -c -2 -Z -IAIL GEN105DE.C
 TLINK @GEN105DE.RES
 
 BCC.EXE -mlarge -O  -c -2 -Z -IAIL GEN300EN.C
 TLINK @GEN300EN.RES
-@REM pause
+pause
