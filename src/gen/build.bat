c65a8da7b2e7d8b7884f8d9d8bfebcafd6a32a53
Henne <119480+Henne@users.noreply.github.com>
2025-06-28 08:42:50 +0200

    NGEN: change build options for BCC

--- src/gen/build.bat
+++ src/gen/build.bat
@@ -3,10 +3,10 @@
 @REM They shall not change, so the are compared with every commit.
 @REM
 @REM -- here start the complete
-BCC.EXE -mlarge -O  -c -2 -Z CDA_CODE.C
-BCC.EXE -mlarge -O  -c -2 -Z -IAIL NGEN.C
-BCC.EXE -mlarge -O  -c -2 -Z VGALIB.C
-@REM BCC.EXE -mlarge -O  -c -2 -Z POWERP20.C
+BCC.EXE -mlarge -O1 -c -1 -Z CDA_CODE.C
+BCC.EXE -mlarge -O1 -c -1 -Z -IAIL NGEN.C
+BCC.EXE -mlarge -O1 -c -1 -Z VGALIB.C
+@REM BCC.EXE -mlarge -O -c -2 -Z POWERP20.C
 TASM.EXE /os /z POWERP20.ASM POWERP20.OBJ
 TASM.EXE /m /w+ /ml /iAIL AIL\AIL.ASM AIL.OBJ
 TLINK @TLINK.RES
