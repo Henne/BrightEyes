2c59532cad573c7109ba25b25fb81d2b7acb0dcb
Henne <119480+Henne@users.noreply.github.com>
2025-07-29 09:54:19 +0200

    SCHICK: sort globvars in datseg.h

--- src/schick/datseg.h
+++ src/schick/datseg.h
@@ -63,10 +63,8 @@ extern signed char g_levelup_ta_rise[12];		//ds:0x2d0f; seg050
 extern signed char g_levelup_sp_rise[6];		//ds:0x2d1b; seg050
 extern signed char g_levelup_spta_conv[6];		//ds:0x2d21; seg050
 extern signed char g_mr_modificators[13];		//ds:0x2d27; seg050
-extern void (*g_location_handlers[19])(void);
-extern void (*g_use_special_item_handlers[14])(void);
-extern void (*g_travel_event_handlers[146])(void);
 
+/* GameState written directly into SaveGame file starts at 0x2d34 */
 extern Bit8s  gs_current_group;			//ds:0x2d35; seg002-seg120
 extern Bit32s gs_day_timer;			//ds:0x2dbb; seg002-seg120
 extern Bit32s gs_ingame_timers[26];		//ds:0x2dc4; seg002, seg042-seg103
@@ -77,9 +75,11 @@ extern Bit8s  gs_dng07_poison_flag;		//ds:0x3cb5; seg082
 extern Bit32s gs_dng07_poison_timer;		//ds:0x3cb6; seg002, seg082
 
 extern Bit32s gs_dng12_watertrap_timer;		//ds:0x3fa2; seg002, seg090
+/* GameState written directly into SaveGame file ends at 0x4474 */
 
 extern char g_game_version[6];			//ds:0x46ec; seg002
 extern signed short g_delay_factor;		//ds:0x4b66; seg005, seg025
+extern void (*g_location_handlers[19])(void);		//ds:0x4c3b; seg025
 
 extern signed short g_fig_all_heroes_withdrawn; //ds:0x5f14; seg032
 extern signed char g_hero_sel_exclude;		//ds:0x64a2; seg047, seg049, seg103, seg120
@@ -88,8 +88,10 @@ extern unsigned char *g_hexenknoten_gfx_buf;	// ds:0xacc8; seg100
 extern signed short g_monster_spell_ae_cost;	// ds:0xaccc; seg102
 extern char g_select_skill_lvlup[6]; 		// ds:0xacce; seg103
 extern signed char g_select_skill_defaults[6];	// ds:0xacd4; seg103
+extern void (*g_use_special_item_handlers[14])(void);	//ds:0xaeb0; seg107
+extern void (*g_travel_event_handlers[146])(void);	//ds:0xaeea; seg109
 
-/* BSS */
+/* BSS starts at 0xbc38, variables in reverse/decreasing order */
 
 extern signed char g_large_buf;		// ds:0xe5e4; seg120, seg028
 extern HugePt g_global_buffer_ptr;	// ds:0xe5e0; seg120
@@ -100,14 +102,16 @@ extern signed short g_textbox_width_bak; // ds:0xe5d5; seg117
 extern signed short g_wallclock_update_bak; // ds:0xe5d3; seg117
 extern unsigned char g_event_ani_busy;	// ds:0xe5d2; seg002, seg109-seg118
 extern unsigned char *g_itemuser;	// ds:0xe5ce; seg107
+extern unsigned char *g_spelluser;	// ds:0xe5bc; seg068, seg098, seg102, seg107
+extern unsigned char *g_spelltarget;	// ds:0xe5b8; seg098-seg102
 
 extern unsigned char *g_dungeon_doors_buf; 	// ds:0xe49c; seg028,seg076,seg098,seg120
 extern unsigned char *g_dungeon_stairs_buf; 	// ds:0xe498; seg028,seg076,seg098,seg120
 extern unsigned char *g_dungeon_fights_buf; 	// ds:0xe494; seg028,seg076,seg098,seg120
 
-extern HugePt g_buffer9_ptr;		// ds:0xc3db; seg004-seg120
 extern HugePt g_buffer9_ptr4;		// ds:0xe3fc; seg028
 extern Bit32s g_fightobj_buf_freespace;	// ds:0xe370; seg032-seg100
+extern unsigned char *g_saved_files_buf;// ds:0xe2d2; seg026;
 extern signed short g_delay_timer;	// ds:0xe2d0; seg004, seg005
 extern Bit32s g_gfx_spinlock;		// ds:0xe234; seg004
 extern char **g_itemsname;		// ds:0xe22f; seg026, seg120
@@ -119,9 +123,7 @@ extern unsigned char *g_mem_slots_mfig;	// ds:0xe11d; seg027, seg120
 extern unsigned char *g_mem_slots_wfig;	// ds:0xe119; seg027, seg120
 extern unsigned char *g_mem_slots_mon;	// ds:0xe115; seg027, seg120
 extern signed short g_wallclock_update;	// ds:0xe113; seg004-seg117, seg120
-extern unsigned char *g_saved_files_buf;// ds:0xe2d2; seg026;
-extern unsigned char *g_spelltarget;	// ds:0xe5b8; seg098-seg102
-extern unsigned char *g_spelluser;	// ds:0xecbc; seg068, seg098, seg102, seg107
+
 extern unsigned char *g_fightobj_buf_seek_ptr; // ds:0xd86e; seg005-seg100
 extern signed char *g_chessboard;	// ds:0xd852;
 extern unsigned char *g_townpal_buf;	// ds:0xd312; seg002, seg027, seg120
@@ -132,32 +134,36 @@ extern signed char *g_text_input_buf;	// ds:0xd2ef: seg002-seg120
 extern signed char *g_text_output_buf;	// ds:0xd2eb: seg002-seg120
 extern unsigned char *g_buf_icon;	// ds:0xd2e7; seg029, seg120
 extern unsigned char *g_objects_nvf_buf;// ds:0xd2e3; seg004, seg032, seg040, seg120
+extern unsigned char *g_fig_figure1_buf;// ds:0xd2df; seg027, seg055, seg056, seg057, seg058, seg120
 extern unsigned char *g_fig_figure2_buf;// ds:0xd2db; seg027, seg040, seg120
-extern unsigned char *g_fig_figure1_buf;// ds:0xd2db; seg027, seg055, seg056, seg057, seg058, seg120
 extern unsigned char *g_buf_font6;	// ds:0xd2c1; seg026, seg096, seg120
+extern unsigned char *g_splash_buffer;	// ds:0xd2bd; seg002, seg028, seg097, seg120
+extern char *g_text_ltx_buffer;		// ds:0xd2b9; seg026, seg120
 extern char *g_buffer7_ptr;		// ds:0xd2b5; seg026, seg028, seg066, seg120
 extern unsigned char *g_buffer6_ptr;	// ds:0xd2b1; seg002, seg027, seg063, seg093, seg094, seg120
-extern char *g_buffer5_ptr;		// ds:0xd2a5; seg026, seg120
-extern unsigned char *g_splash_buffer;	// ds:0xbcc7; seg002, seg028, seg097, seg120
-extern char *g_text_ltx_buffer;		// ds:0xd2b9; seg026, seg120
 extern unsigned char *g_popup;		// ds:0xd2ad; seg027, seg096, seg120
+extern char *g_buffer5_ptr;		// ds:0xd2a5; seg026, seg120
 extern char *g_monnames_buffer;		// ds:0xd2a1; seg026, seg120
 extern unsigned char *g_trv_track_pixel_bak;	// ds:0xd299; seg063, seg094, seg120
 extern HugePt g_buffer9_ptr3;		// ds:0xd019; seg003-seg120
 extern unsigned char *g_buffer9_ptr2;	// ds:0xd015; seg120
+
 extern Bit32s g_ani_unknown4;		// ds:0xce43; seg027, seg120
+extern HugePt g_buffer9_ptr;		// ds:0xc3db; seg004-seg120
 extern char **g_text_ltx_index;		// ds:0xc3b5;
 extern char **g_tx_index;		// ds:0xc3b1; seg026, seg028, seg031, seg064, seg120
 extern char **g_tx2_index;		// ds:0xc3ad; seg
 extern unsigned char *g_buffer8_ptr;	// ds:0xc3a9; seg005, seg026, seg028, seg032, seg042, seg050, seg061, seg098, seg120
+
 extern signed short g_textbox_width;	// ds:0xbffd; seg002-seg120
-extern unsigned char *g_gui_buffer_unkn;//ds:0xbff9; seg025, seg032, seg097, seg120
-extern Bit32s g_archive_file_remaining;	//ds:0xbce3; seg002
+extern unsigned char *g_gui_buffer_unkn;// ds:0xbff9; seg025, seg032, seg097, seg120
+extern unsigned char *g_heroes;		// ds:0xbd34
+extern unsigned char *g_fightobj_buf;	// ds:0xbd30; seg032, seg040
+extern Bit32s g_archive_file_remaining;	// ds:0xbce3; seg002
 extern unsigned char *g_splash_le;	// ds:0xbccb; seg002, seg028
 extern unsigned char *g_splash_ae;	// ds:0xbcc7; seg002, seg028
 extern unsigned char g_gfx_wait_keypress[100];	// ds:0xbc63; seg002
 extern unsigned char g_playmask_us;	// ds:0xbc62; seg002, seg029
-extern unsigned char *g_heroes;		// ds:0xbd34
 
 #if !defined(__BORLANDC__)
 /* arrays for meaningful log messages */
