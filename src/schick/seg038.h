0a8f3e14c307f31b7d820de7f4dcd5bb827c9e1d
Henne <119480+Henne@users.noreply.github.com>
2025-11-19 09:52:51 +0100

    SCHICK + SCHICK_DOS: minor changes in seg038

--- src/schick/seg038.h
+++ src/schick/seg038.h
@@ -8,15 +8,15 @@ signed int FIG_search_obj_on_cb(const signed int, signed int*, signed int*);
 void FIG_init_list_elem(const signed int);
 /* 0x2a */
 #if defined(__BORLANDC__)
-/* static */
+/* should be static */
 void FIG_unused(const signed int, const signed int, signed int*, signed int*);
 #endif
 /* 0x2f */
-/* static */
-void FIG_find_path_to_target_backtrack(uint8_t*, signed int, signed int, signed int, const signed char, const signed int, const signed int, const signed int);
+/* should be static */
+void FIG_find_path_to_target_backtrack(int8_t*, signed int, signed int, signed int, const signed char, const signed int, const signed int, const signed int);
 /* 0x34 */
-/* static */
-signed int FIG_count_direction_changes_of_path(signed char*);
+/* should be static */
+signed int FIG_count_direction_changes_of_path(const int8_t*);
 /* 0x39 */
 signed int FIG_find_path_to_target(uint8_t *, const signed int, const signed int, const signed int, const signed int);
 
