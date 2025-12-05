1680720f436c53428ff569a6103a2edd0ee1dfd6
siebenstreich <siebenstreich@gmx.net>
2025-12-05 11:32:46 +0100

    SCHICK_DOS, SCHICK: correct minor typo in comment

--- src/schick/seg119.cpp
+++ src/schick/seg119.cpp
@@ -606,7 +606,7 @@ void disease_effect(void)
 			for (j = 1; j <= 7; j++) {
 
 				if (hero->disease[j].status != DISEASE_STATUS_HEALTHY) {
-					/* Original-Bug? I think for some of the deseases, an overflow could theoretically happen. */
+					/* Original-Bug? I think for some of the diseases, an overflow could theoretically happen. */
 					hero->disease[j].time_counter++;
 				}
 			}
