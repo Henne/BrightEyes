9b82e2f1e7633c7be579f46dbd0ee0a8f2fc8f25
siebenstreich <siebenstreich@gmx.net>
2025-12-14 23:53:07 +0100

    SCHICK_DOS, SCHICK: add missing keyword 'struct'

--- src/schick_dos/datseg.h
+++ src/schick_dos/datseg.h
@@ -2041,7 +2041,7 @@ extern char **g_tx_index;		// ds:0xc3b1; seg026, seg028, seg031, seg064, seg120
 extern char **g_tx2_index;		// ds:0xc3ad; seg
 extern unsigned char *g_buffer8_ptr;	// ds:0xc3a9; seg005, seg026, seg028, seg032, seg042, seg050, seg061, seg098, seg120
 extern struct location g_locations_tab[150];	//ds:0xc025; seg028, seg64, seg066, seg74, seg094
-extern struct_pic_copy g_pic_copy;	// ds:0xc00d; seg002-seg120
+extern struct struct_pic_copy g_pic_copy;	// ds:0xc00d; seg002-seg120
 
 extern struct item_selector_item *g_item_selector_buy; /* used for merchant buy screen */	// ds:0xc009; seg055, seg056
 extern struct item_selector_item *g_item_selector_sell;	/* used for merchant sell screen and smith */ // ds:0xc005; seg056, seg057, seg058
